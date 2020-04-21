b7c7966ee34cc004fb043b8dc38105d8854bc446
rgnaldo1@gmail.com <rgnaldo1@gmail.com>
2020-04-21 18:46:08 -0400

    configurando o sucrase e nodemon

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import app from './app';
 =======
 const app = require('./app');
 >>>>>>> Configuração inicial do servidor
+=======
+import app from './app';
+>>>>>>> configurando o sucrase e nodemon
 
 app.listen(3333)
